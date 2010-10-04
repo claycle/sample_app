628b0b10b9b787414d67baf14574fd79cf720890
Clay Luther <claycle@gmail.com>
2010-10-04 17:05:59 -0500

    Exercises 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Return the logo
+  # 	<% logo = image_tag("logo.png", :alt => "Sample App", :class => "round") %>
+  def logo
+    image_tag("logo.png",  :alt => "Sample App", :class => "round")
+  end
 end
